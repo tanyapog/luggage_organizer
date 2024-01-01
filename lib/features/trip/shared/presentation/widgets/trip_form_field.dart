390a904639194ee72d500124710c0a3f5c18bc5d
tanyapog <megatatb@yandex.ru>
2024-01-01 16:14:55 +0300

    see #7 Simplified TripFormField to StatelessWidget because controller was needed only for setting initial value and now, when is it given to the TripFormBloc as a parameter, controller became redundant.

--- lib/features/trip/shared/presentation/widgets/trip_form_field.dart
+++ lib/features/trip/shared/presentation/widgets/trip_form_field.dart
@@ -5,7 +5,7 @@ import '../../../../../global/presentation/theme/app_input.dart';
 import '../../../../../global/presentation/theme/app_padding.dart';
 import '../../../../../global/presentation/theme/app_text.dart';
 
-class TripFormField extends StatefulWidget {
+class TripFormField extends StatelessWidget {
   const TripFormField({
     super.key, 
     this.initialValue, 
@@ -22,46 +22,18 @@ class TripFormField extends StatefulWidget {
   final int? maxLines;
 
   @override
-  State<TripFormField> createState() => _TripFormFieldState();
-}
-
-class _TripFormFieldState extends State<TripFormField> {
-  final controller = TextEditingController();
-
-  @override
-  void initState() {
-    controller.text = widget.initialValue ?? "";
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  void didUpdateWidget(covariant TripFormField oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.initialValue != widget.initialValue) {
-      controller.text = widget.initialValue ?? "";
-    }
-    super.didUpdateWidget(oldWidget);
-  }
-
-  @override
   Widget build(BuildContext context) {
     return TextFormField(
-      maxLines: widget.maxLines,
+      initialValue: initialValue,
+      onChanged: onChanged,
+      validator: validator,
+      maxLines: maxLines,
       minLines: 1,
       maxLength: 70,
-      validator: widget.validator,
-      onChanged: widget.onChanged,
-      controller: controller,
       style: AppTextStyle.titleSmall.copyWith(color: AppColors.grey90),
       decoration: AppInputDecoration.noBorder.copyWith(
         contentPadding: AppPadding.vertical15,
-        hintText: widget.hintText,
+        hintText: hintText,
       ),
     );
   }
